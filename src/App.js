7122d5db94f8f34af3d5da83948dd98ef46092b9
Jo Sands <jozie.sands@gmail.com>
2019-12-16 17:18:57 -0600

    fetch testdata from database

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import NameBlock from './NameBlock';
-import testData from './testData.json';
 
 function App() {
 
   const billGatesTotal = 6379000000;
-  let leftover = billGatesTotal;
 
-  const data = testData.people;
+  const [submissions, setSubmissions] = useState([]);
+  const [totalDebt, setTotalDebt] = useState(0);
 
-  function getLeftover(){
-    data.forEach((person) => {
-      leftover -= person.debt;
-    });
 
-  }
+  useEffect(() => {
+    fetch("https://tpkfcvx8jf.execute-api.us-east-1.amazonaws.com/dev/submissions")
+      .then((res) => res.json())
+      .then((data) =>{
+        setTotalDebt(data[0]['total_debt'])
+        setSubmissions(data[0]['submissions'])
+      })
+    }, [])
 
-  getLeftover();
+    function moneyLeft(){
+      return(billGatesTotal-totalDebt);
+    }
 
   return (
     <div className="App">      
@@ -25,10 +29,10 @@ function App() {
         <h1>Bill Gates would pay $6.379 billion next year under Elizabeth's wealth tax.</h1>
         <h3>Thats enough money to pay off student loan debt for ...</h3>
       </div>
-      <NameBlock data={data}/>
+        <NameBlock data={submissions}/>
       <div>
-        {leftover > 0 && 
-        <h3>... and still have ${leftover.toLocaleString()} leftover.</h3>
+        {moneyLeft() > 0 && 
+        <h3>... and still have ${moneyLeft().toLocaleString()} left over.</h3>
         }
         
         <p>Elizabeth's wealth tax, which only impacts America's 75,000 wealthiest families, would generate enough revenue to cover universal child care, quality public education, forgive student loan debt, provide free public college, and help finance Medicare for All.</p>
