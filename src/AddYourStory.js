0137c72d8e8cdd7afc3ed462afea56effe4bd636
Jo Sands <jozie.sands@gmail.com>
2019-12-15 18:03:39 -0600

    add form validation

--- src/AddYourStory.js
+++ src/AddYourStory.js
@@ -8,6 +8,12 @@ export function AddYourStory(props){
     const [storyInput, setStoryInput] = useState("");
     const [emailInput, setEmailInput] = useState("");
 
+    const [validName, setValidName] = useState(false);
+    const [validDebt, setValidDebt] = useState(false);
+    const [validEmail, setValidEmail] = useState(false);
+
+    const isEnabled = validName && validDebt && validEmail;
+
     const [isModalVisible, setIsModalVisible] = useState(false);
         
     function toggleModal(){
@@ -30,6 +36,13 @@ export function AddYourStory(props){
     
     function cleanDebtInput(event){
         let cleanValue = event.target.value.replace(/\D*/g, '');
+        if(cleanValue.length > 0){
+            setValidDebt(true);
+            event.target.classList.remove('error')
+        } else {
+            setValidDebt(false)
+            event.target.classList.add('error');
+        }
         setDebtInput(cleanValue);
     }
 
@@ -41,11 +54,21 @@ export function AddYourStory(props){
 
         <div id="AYSname">
             <label htmlFor="name">Name</label>
-            <input id="name" value={nameInput} onChange={(event) => setNameInput(event.target.value)}></input>
+            <input id="name" value={nameInput} onChange={(event) => {
+                if(event.target.value.length > 0){
+                    setValidName(true);
+                    event.target.classList.remove('error')
+                } else {
+                    setValidName(false);
+                    event.target.classList.add('error')
+                }
+                setNameInput(event.target.value)
+            }}>
+            </input>
         </div>
         <div id="AYSdebt">
             <label htmlFor="debt">Student Loan Debt</label>
-            <input id="debt" value={debtInput} onChange={cleanDebtInput} maxLength='7' minLength='1'></input>
+            <input id="debt" value={debtInput} maxLength='6' onChange={cleanDebtInput} ></input>
         </div>
         <div id='AYSstory'>
             <label htmlFor="story">Your Story</label>
@@ -53,9 +76,19 @@ export function AddYourStory(props){
         </div>
         <div id='AYSemail'>
             <label htmlFor="email">Email</label>
-            <input id="email" value={emailInput} onChange={(event) => setEmailInput(event.target.value)}></input>
+            <input id="email" value={emailInput} onChange={(event) => {
+                    setEmailInput(event.target.value)
+                    if(event.target.value.includes('@')){
+                        setValidEmail(true);
+                        event.target.classList.remove('error');
+                    } else {
+                        setValidEmail(false);
+                        event.target.classList.add('error');
+                    }
+                }}>
+            </input>
         </div>
-        <button id="AYSsubmit" onClick={onSubmit}>Submit</button>
+        <button id="AYSsubmit" disabled={!isEnabled} onClick={onSubmit}>Submit</button>
 
         { isModalVisible && (
             <Modal onModalClose={() => {
