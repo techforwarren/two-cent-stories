cfa5049f8f3f3177d2f27a4345f0f69924437533
Daniel Narey <danielnarey@users.noreply.github.com>
2019-12-15 23:33:06 -0500

    text sizing
    - added text size classes to App.css
    - made corresponding changes to top-level markup in App.js
    - added horizontal padding to layout classes
    - set x/y overflow rules on html root

--- src/App.js
+++ src/App.js
@@ -23,8 +23,8 @@ function App() {
     <div className="App">      
       <div className="App-header">
         <div className="App-container">
-          <h1>Bill Gates would pay $6.379 billion next year under Elizabeth’s wealth tax.</h1>
-          <h3>That’s enough money to pay off student loan debt for ...</h3>
+          <h1 className="text-xl">Bill Gates would pay $6.379 billion next year under Elizabeth’s wealth tax.</h1>
+          <p className="text-lg">That’s enough money to pay off student loan debt for ...</p>
         </div>
       </div>
 
@@ -38,11 +38,13 @@ function App() {
         <div className="App-container">
           <div>
             {leftover > 0 && 
-            <h3>... and still have ${leftover.toLocaleString()} leftover.</h3>
+            <p className="text-lg">... and still have ${leftover.toLocaleString()} leftover.</p>
             }
             
-            <p>Elizabeth’s wealth tax, which only impacts America’s 75,000 wealthiest families, would generate enough revenue to cover universal child care, quality public education, forgive student loan debt, provide free public college, and help finance Medicare for All.</p>
-            <a href="https://elizabethwarren.com/plans/ultra-millionaire-tax">Find out more about the Wealth tax and join the fight</a>
+            <p className="text-base">Elizabeth’s wealth tax, which only impacts America’s 75,000 wealthiest families, would generate enough revenue to cover universal child care, quality public education, forgive student loan debt, provide free public college, and help finance Medicare for All.</p>
+            <p className="text-base">
+              <a href="https://elizabethwarren.com/plans/ultra-millionaire-tax">Find out more about the Wealth tax and join the fight</a>
+            </p>
           </div>
         </div>
       </div>
