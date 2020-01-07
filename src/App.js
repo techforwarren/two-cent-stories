004973a1fef3dfa9e9c073d358f54cafd471c1ed
Jo Sands <jozie.sands@gmail.com>
2020-01-06 20:14:32 -0600

    fix skipped heading number and modal exit drops focus on correct element

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
     <div className="App">      
       <div>
         <h1>Bill Gates would pay $6.379 billion next year under Elizabeth's wealth tax.</h1>
-        <h3>That's enough money to pay off student loan debt for ...</h3>
+        <h2>That's enough money to pay off student loan debt for ...</h2>
       </div>
         <NameBlock data={submissions}/>
       <div>
