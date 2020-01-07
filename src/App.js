7c2e5f5330b42d933f9a2786c5e6d3e2571f93eb
Daniel Narey <danielnarey@users.noreply.github.com>
2020-01-06 23:34:38 -0500

    sync

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ function App() {
             }
             
             <p className="text-base">Elizabeth’s wealth tax, which only impacts America’s 75,000 wealthiest families, would generate enough revenue to cover universal child care, quality public education, forgive student loan debt, provide free public college, and help finance Medicare for All.</p>
-            <p className="text-base text-center">
+            <p className="text-base text-right">
               <a href="https://elizabethwarren.com/plans/ultra-millionaire-tax">Find out more about the wealth tax</a> and <a href="https://elizabethwarren.com/join-us">join the fight</a>
             </p>
             <div className="disclaimer">
